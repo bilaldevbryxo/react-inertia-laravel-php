1e270364407d8b4a037ed2a12a88647a90d88ccc
bilal544 <mbilaldev23@gmail.com>
2025-11-19 22:45:36 +0500

    djfhsk

---
+++ resources/js/app.jsx
@@ -0,0 +1,11 @@
+import './bootstrap';
+import { createRoot } from 'react-dom/client';
+import { createInertiaApp } from '@inertiajs/react';
+
+createInertiaApp({
+    resolve: name => import(`./Pages/${name}.jsx`),
+    setup({ el, App, props }) {
+        const root = createRoot(el);
+        root.render(<App {...props} />);
+    },
+});
